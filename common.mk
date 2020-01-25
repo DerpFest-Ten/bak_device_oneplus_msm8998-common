95f65dd627188b3b7bbcfe76386351aa5eaa78e3
NurKeinNeid <mralexman3000@gmail.com>
2020-01-25 18:26:54 +0100

    msm8998-common: DerpFest bringup
     * Update extract-utils
     * Update dependencies
     * Modify overlays
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- common.mk
+++ common.mk
@@ -27,7 +27,9 @@ $(call inherit-product, vendor/addons/onepluscamera/config.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-derp
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
